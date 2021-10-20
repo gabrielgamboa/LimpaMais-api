34882686c772ef73a9d2e79967cd1c30031909df
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-10-19 21:29:52 -0300

    adicionando porta do servidor

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 3333, () => console.log(`Server is running on port ${process.env.PORT}`));
