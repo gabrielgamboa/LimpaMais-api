9966155002bfdcfb1d0401521955c3882974510b
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-12-02 21:01:19 -0300

    criado método para encontrar o serviço pelo seu id

--- src/controllers/ServiceController.js
+++ src/controllers/ServiceController.js
@@ -1,19 +1,49 @@
-const { Service } = require("../models");
+const { Service, User, Diarist } = require("../models");
 class ServiceController {
     async create(request, response) {
-        const { user_id, diarist_id, appointment_date } = request.body;
+
+        const { user_id, diarist_id } = request.body;
 
         try {
             await Service.create({
                 user_id,
                 diarist_id,
-                appointment_date
+                appointment_date: new Date(),
+                status: "created"
             });
 
             return response.status(201).send();
 
         } catch (error) {
             return response.status(500).send(error)
+        }
+    }
+
+    async findServiceById(request, response) {
+        const { id } = request.params;
+
+        try {
+            const service = await Service.findOne({
+                where: { id },
+                include: [
+                    {
+                        model: User,
+                        as: "user",
+                        attributes: ["name", "email", "phone", "street", "number", "city", "state"]
+                    },
+                    {
+                        model: Diarist,
+                        as: "diarist",
+                        attributes: ["name", "email", "phone", "street", "number", "city", "state", "daily_rate", "note"]
+                    }
+                ]
+            });
+
+            return response.json(service);
+
+
+        } catch (error) {
+            console.log(error)
         }
     }
 }
--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
+const { ServiceController } = require("../controllers/ServiceController");
 
 const servicesRoutes = Router();
 
+const serviceController = new ServiceController();
 
+servicesRoutes.post("/create", serviceController.create);
+servicesRoutes.get("/:id", serviceController.findServiceById);
 
 module.exports = { servicesRoutes }
